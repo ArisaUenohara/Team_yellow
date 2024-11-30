228f6084b0213bd8350fc6d6970d05d32a5a4501
ArisaUenohara <8923018@ed.tus.ac.jp>
2024-11-30 19:56:20 +0900

    Delete newpage directory

--- newpage/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//ini_set('display_errors', 1);
-
-define('DSN', 'mysql:host=mysql1025.onamae.ne.jp;dbname=57hwa_student');
-define('DB_USER', '57hwa_student');
-define('DB_PASS', 'Ixia2leaf!');
